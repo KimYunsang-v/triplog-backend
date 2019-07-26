3bd3b9ef806f990e9ccfe26d94d91009b57f39e9
kimYSang <qwdbstkd@naver.com>
2019-07-26 21:16:23 +0900

    Initial commit

---
+++ src/main/java/kr/ac/skunive/triplog/TriplogApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.skunive.triplog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriplogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriplogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/skunive/triplog/TriplogApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.ac.skunive.triplog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TriplogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
